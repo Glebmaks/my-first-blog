9e32556902faf4f8b26569f0a2a2e2ad959b7d77
glebmaks <gleb.maksimenkov@gmail.com>
2024-03-24 05:09:13 +0300

    The main app is ready, pages index and about

--- main/views.py
+++ main/views.py
@@ -1,15 +1,23 @@
-from urllib import request
+
+
 from django.http import HttpResponse
 from django.shortcuts import render
 
 def index(request):
     context: dict = {
-        'title': 'Home',
-        'content': 'Главная страница магазина'
+        'title': 'Home - Главная',
+        'content': 'Магазин мебели HOME'
 
     }
 
     return render(request, 'main/index.html', context)
 
 def about(request):
-    return HttpResponse('About page')
+     context: dict = {
+        'title': 'Home - О нас',
+        'content': 'О нас',
+        'text_on_page': "Текст"
+
+    }
+     
+     return render(request, 'main/about.html', context)
