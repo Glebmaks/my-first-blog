5c17072d0331b83319c1a910b6def6ebeeb05474
glebmaks <gleb.maksimenkov@gmail.com>
2024-04-24 00:29:11 +0300

    Regidtration form and controller are ready

--- users/forms.py
+++ users/forms.py
@@ -14,3 +14,24 @@ class UserLoginForm(AuthenticationForm):
 
     username = forms.CharField()
     password = forms.CharField()
+
+
+class UserRegistrationForm(UserCreationForm):
+
+    class Meta:
+        model = User
+        fields = (
+            "first_name",
+            "last_name",
+            "username",
+            "email",
+            "password1",
+            "password2",
+        )
+    
+    first_name = forms.CharField()
+    last_name = forms.CharField()
+    username = forms.CharField()
+    email = forms.CharField()
+    password1 = forms.CharField()
+    password2 = forms.CharField()
